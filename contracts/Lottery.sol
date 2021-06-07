2b9d0e3f4eb63be0ddc9b7e2ac042bfd5a5bc272
Pedro Costa <61250854+capitanwesler@users.noreply.github.com>
2021-06-07 11:56:03 +0100

    Adding the mock for the VRF Number

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@chainlink/contracts/src/v0.8/dev/VRFConsumerBase.sol";
 
 
 contract Lottery is Initializable {
