05ba70691b0d34ea95560252cc07f451bafbad07
capitanwesler <guillermo.campanudo@hotmail.com>
2021-06-09 00:38:59 +0100

    Adding the capability to add aggregators to the contract and get USD

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./interfaces/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Lottery is Initializable, ContextUpgradeable {
@@ -25,7 +27,17 @@ contract Lottery is Initializable, ContextUpgradeable {
     parameter on the initializer to stablish the admins
     of the contract.
   **/
-  mapping(address => bool) admins;
+  mapping(address => bool) public admins;
+
+  /**
+    @dev This is the mapping of aggregators. 
+    @notice It will check if an address has 
+    the address of the aggregator to check
+    the price in USD from this address, if the address
+    to check the price in `USD` is equal to the address 
+    in the `aggregators` that mean it's a stablecoin.
+  **/
+  mapping(address => address) internal aggregators;
 
 
   /** 
@@ -38,21 +50,6 @@ contract Lottery is Initializable, ContextUpgradeable {
     uint256 ticketsBuyed,
     address indexed lendingPool
   );
-
-  /**
-    @notice Defining the price feed, and to see what is the user going
-    to pay.
-    @dev These are our stable coins and ETH.
-  **/
-
-  enum TicketPrice {
-    ETH,
-    DAI,
-    USDC,
-    USDT,
-    TUSD,
-    BUSD
-  }
   
   /** 
     @dev Initializer of the function, here we will set
@@ -73,7 +70,7 @@ contract Lottery is Initializable, ContextUpgradeable {
     @dev Modifier, to check if either the actual user is an admin or not.
   **/
   modifier onlyAdmin() {
-    require(admins[_msgSender()], "ADMIN ERROR: You aren't an admin to do this.");
+    require(admins[_msgSender()], "Admin: NOT_ADMIN");
     _;
   }
 
@@ -82,9 +79,40 @@ contract Lottery is Initializable, ContextUpgradeable {
     @notice This is for changing the price of the ticket,
     if we want to change the price of the tickets after a
     lottery has pass.
+    @param _ticketCost This is the ticketCost and it can be only set by an admin.
   **/
   function setTicketCost(uint256 _ticketCost) external onlyAdmin returns(bool success) {
+    require(_ticketCost != 0, "TicketCost: ERROR_TICKET_COST");
     ticketCost = _ticketCost;
     success = true;
+  }
+
+  /**
+    @dev Function to add the aggregator to check the chainlink aggregator.
+    @param _token This is the token what we want to get the `USD` price.
+    @param _aggregator This is the aggregator to check the `USD` price.
+  **/
+
+  function _addAggregator(address _token, address _aggregator) external onlyAdmin {
+    require(_token != address(0) && _aggregator != address(0), "Aggregator: ZERO_ADDRESS");
+    aggregators[_token] = _aggregator;
+  }
+
+  /**
+    @dev Returns the price of a token in USD.
+    @param _tokenPayment Address of the ERC-20 Token.
+  */
+  function _getPriceByToken(address _tokenPayment)
+    internal
+    view
+    returns (uint256)
+  {
+    require(
+        aggregators[_tokenPayment] != address(0),
+        "Aggregator: ZERO_ADDRESS"
+    );
+    (,int256 price,,,) = AggregatorV3Interface(aggregators[_tokenPayment]).latestRoundData();
+
+    return uint256(price);
   }
 }
