87228e53ea5c48bc23df8ca413d6108fe9bc268b
capitanwesler <guillermo.campanudo@hotmail.com>
2021-06-13 11:22:59 +0100

    Making a swapper in the lottery for tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -11,39 +11,39 @@ const EXCHANGE = '0xD1602F68CC7C4c7B59D686243EA35a9C73B0c6a2';
   and LINK tokens in the forked mainnet.
 */
 
-// describe('Swapping: ETH for Tokens', () => {
-//   let swapper;
-//   let DAItoken;
-//   let LINKtoken;
+describe('Swapping: ETH for Tokens', () => {
+  let swapper;
+  let DAItoken;
+  let LINKtoken;
 
-//   before(async () => {
-//     // - Getting the factories for the contracts:
-//     const Swapper = await ethers.getContractFactory('Swapper');
+  before(async () => {
+    // - Getting the factories for the contracts:
+    const Swapper = await ethers.getContractFactory('Swapper');
 
-//     const accounts = await ethers.getSigners();
+    const accounts = await ethers.getSigners();
 
-//     swapper = await Swapper.deploy(accounts[0].address);
-//     await swapper.deployed();
+    swapper = await Swapper.deploy(accounts[0].address);
+    await swapper.deployed();
 
-//     // DAI TOKEN
-//     DAItoken = await ethers.getContractAt('IERC20', DAI);
+    // DAI TOKEN
+    DAItoken = await ethers.getContractAt('IERC20', DAI);
 
-//     // LINK TOKEN
-//     LINKtoken = await ethers.getContractAt('IERC20', LINK);
-//   });
+    // LINK TOKEN
+    LINKtoken = await ethers.getContractAt('IERC20', LINK);
+  });
 
-//   it('change ETH for multiple tokens', async () => {
-//     const porcents = [50 * 10, 50 * 10];
-//     const tokens = [
-//       '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI Token
-//       '0x514910771AF9Ca656af840dff83E8264EcF986CA', // LINK Token
-//     ];
+  it('change ETH for multiple tokens', async () => {
+    const porcents = [50 * 10, 50 * 10];
+    const tokens = [
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI Token
+      '0x514910771AF9Ca656af840dff83E8264EcF986CA', // LINK Token
+    ];
 
-//     await swapper.swapEthForTokens(tokens, porcents, {
-//       value: ethers.utils.parseEther('5'),
-//     });
-//   });
-// });
+    await swapper.swapEthForTokens(tokens, porcents, {
+      value: ethers.utils.parseEther('2'),
+    });
+  });
+});
 
 describe('Testing: Lottery Contract', async () => {
   let accounts;
@@ -54,7 +54,6 @@ describe('Testing: Lottery Contract', async () => {
   let alarmClock;
   let iERC20Dai;
   let iERC20USDT;
-  let iStableSwap;
 
   before(async () => {
     // - Getting the factories for the contracts:
